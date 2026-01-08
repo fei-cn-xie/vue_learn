bad437c556ce1eb5bc9adf1afc39805890a411cd
fei's AMD Windows <fei.cn.xie@gmail.com>
2026-01-08 22:42:23 +0800

    v-model方式传递值及其底层原理

--- src/pages/04_v-model/AtguiguInput.vue
+++ src/pages/04_v-model/AtguiguInput.vue
@@ -1,5 +1,19 @@
 <template>
-  <input 
+
+  <!-- 2 v-model 用在组件标签上 --> 
+   <input 
+   type="text"
+   :value="modelValue"
+   @input="emit('update:modelValue', (<HTMLInputElement>$event.target).value)"
+   />
+   <!-- $event到底是什么？啥时候能$event.target -->
+    <!-- 1. 对于原生事件，$event就是事件对象 ==> 能`.target`-->
+    <!-- 2. 对于自定义事件, $event就是触发事件时，所传递的数据不能`.target` -->
+
+
+
+   <!-- 3  -->
+  <!-- <input 
     type="text" 
     :value="ming"
     @input="emit('update:ming',(<HTMLInputElement>$event.target).value)"
@@ -9,12 +23,18 @@
     type="text" 
     :value="mima"
     @input="emit('update:mima',(<HTMLInputElement>$event.target).value)"
-  >
+  > -->
 </template>
 
 <script setup lang="ts" name="AtguiguInput">
-  defineProps(['ming','mima'])
-  const emit = defineEmits(['update:ming','update:mima'])
+
+  // 2 v-model用在组件标签上
+  defineProps(['modelValue'])
+  const emit = defineEmits(['update:modelValue'])
+
+
+  // defineProps(['ming','mima'])
+  // const emit = defineEmits(['update:ming','update:mima'])
 </script>
 
 <style scoped>
--- src/pages/04_v-model/Father.vue
+++ src/pages/04_v-model/Father.vue
@@ -3,19 +3,20 @@
     <h3>父组件</h3>
     <h4>{{ username }}</h4>
     <h4>{{ password }}</h4>
-    <!-- v-model用在html标签上 -->
-    <!-- <input type="text" v-model="username"> -->
-    <!-- <input type="text" :value="username" @input="username = (<HTMLInputElement>$event.target).value"> -->
+    <!--1 v-model用在html标签上 -->
+    <!-- 方式一： <input type="text" v-model="username"> 两种方式等价 -->
+    <!-- 方式二：<input type="text" :value="username" @input="username = (<HTMLInputElement>$event.target).value"> -->
 
-    <!-- v-model用在组件标签上 -->
-    <!-- <AtguiguInput v-model="username"/> -->
-    <!-- <AtguiguInput 
+    <!--2 v-model用在组件标签上 -->
+     <AtguiguInput v-model="username"/>
+    <!--底层写法： <AtguiguInput 
       :modelValue="username" 
       @update:modelValue="username = $event"
     /> -->
 
+    <!-- 3 -->
     <!-- 修改modelValue -->
-    <AtguiguInput v-model:ming="username" v-model:mima="password"/>
+    <!-- <AtguiguInput v-model:ming="username" v-model:mima="password"/> -->
   </div>
 </template>
 
@@ -23,7 +24,7 @@
 	import { ref } from "vue";
   import AtguiguInput from './AtguiguInput.vue'
   // 数据
-  let username = ref('zhansgan')
+  let username = ref('zs123')
   let password = ref('123456')
 </script>
 
